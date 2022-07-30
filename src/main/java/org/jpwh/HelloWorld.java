4ae0edc0e47a1adb3dbb67972d48737d1e4f6c21
Yaroslav Agapov <agapovi56@gmail.com>
2022-07-30 20:53:51 +0500

    Read to 172 page

--- src/main/java/org/jpwh/HelloWorld.java
+++ src/main/java/org/jpwh/HelloWorld.java
@@ -4,13 +4,15 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.jpwh.model.inheritance.tableperclass.BankAccount;
-import org.jpwh.model.inheritance.tableperclass.BillingDetails;
-import org.jpwh.model.inheritance.tableperclass.CreditCard;
+import org.jpwh.model.inheritance.associations.manytoone.BankAccount;
+import org.jpwh.model.inheritance.associations.manytoone.BillingDetails;
+import org.jpwh.model.inheritance.associations.manytoone.CreditCard;
+import org.jpwh.model.inheritance.associations.manytoone.User;
+
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.List;
+
 import java.util.Properties;
 
 public class HelloWorld {
@@ -31,13 +33,16 @@ public class HelloWorld {
              Session session = sessionFactory.openSession()) {
             System.out.println("Ok");
             Transaction tx = session.beginTransaction();
-//            BankAccount bankAccount = new BankAccount("Owner","account","Sber","Swift");
-//            CreditCard creditCard = new CreditCard("Owner","1111 1111 1111 1111","09","1991");
-            BankAccount bankAccount = new BankAccount();
-            CreditCard creditCard = new CreditCard(bankAccount.getOwner(),"111","09/09","2022");
-            List billingDetails = session.createQuery("select bd from BillingDetails bd").getResultList();
+            User user = new User("Yurii");
+            BankAccount bankAccount = new BankAccount("Owner","account","BankName","SWIFT");
+            CreditCard creditCard = new CreditCard("Card Owner","0000 0000 0000 0000","09","22");
+
+            user.setDefaultBilling(bankAccount);
+           // user.setDefaultBilling(creditCard);
             session.persist(bankAccount);
             session.persist(creditCard);
+            session.persist(user);
+
 
             tx.commit();
             System.out.println("+");
