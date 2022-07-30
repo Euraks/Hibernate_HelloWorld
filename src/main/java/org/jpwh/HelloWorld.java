3f147db97352d601583cea095910b025c93b46b4
Yaroslav Agapov <agapovi56@gmail.com>
2022-07-30 21:01:08 +0500

    Set<String>

--- src/main/java/org/jpwh/HelloWorld.java
+++ src/main/java/org/jpwh/HelloWorld.java
@@ -4,16 +4,13 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.jpwh.model.inheritance.associations.manytoone.BankAccount;
-import org.jpwh.model.inheritance.associations.manytoone.BillingDetails;
-import org.jpwh.model.inheritance.associations.manytoone.CreditCard;
-import org.jpwh.model.inheritance.associations.manytoone.User;
-
+import org.jpwh.model.collections.setofstrings.Item;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -33,16 +30,12 @@ public class HelloWorld {
              Session session = sessionFactory.openSession()) {
             System.out.println("Ok");
             Transaction tx = session.beginTransaction();
-            User user = new User("Yurii");
-            BankAccount bankAccount = new BankAccount("Owner","account","BankName","SWIFT");
-            CreditCard creditCard = new CreditCard("Card Owner","0000 0000 0000 0000","09","22");
-
-            user.setDefaultBilling(bankAccount);
-           // user.setDefaultBilling(creditCard);
-            session.persist(bankAccount);
-            session.persist(creditCard);
-            session.persist(user);
-
+            Item item = new Item();
+            Set<String> stringSet = new HashSet<>();
+            stringSet.add("FirstImage");
+            stringSet.add("SecondString");
+            item.setImages(stringSet);
+            session.persist(item);
 
             tx.commit();
             System.out.println("+");
